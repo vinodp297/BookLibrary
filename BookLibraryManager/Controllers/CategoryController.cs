de5cd7b537c0a409c80b7ff2b8fec08be8feb67e
Vinod Philip <vinodp297@gmail.com>
2023-07-15 08:49:00 -0700

    Added validations message completed CRUD operation deployed in Azure

--- BookLibraryManager/Controllers/CategoryController.cs
+++ BookLibraryManager/Controllers/CategoryController.cs
@@ -18,5 +18,123 @@ namespace BookLibraryManager.Controllers
             IEnumerable<Category> categoryList = _dbContext.Categories;
             return View(categoryList);
         }
+
+        // Create Category : GET
+        public IActionResult CreateCategory()
+        {
+            return View();
+        }
+
+        // Create Category : Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult CreateCategory(Category category)
+        {
+            if (category.Name == category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("CustomError", "Value for Name and Display can't be same.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _dbContext.Categories.Add(category);
+                _dbContext.SaveChanges();
+                TempData["success"] = "Category added successfully.";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                TempData["error"] = "Error occured, try again.";
+                return View(category);
+            }
+
+        }
+
+        // Create Category : GET
+        public IActionResult EditCategory(int? categoryId)
+        {
+            if (categoryId == null || categoryId == 0)
+            {
+                return NotFound();
+            }
+
+            var categoryList = _dbContext.Categories.FirstOrDefault(x => x.Id == categoryId);
+
+            if (categoryList == null)
+            {
+                return NotFound();
+            }
+            return View(categoryList);
+        }
+
+        // Create Category : Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult EditCategory(int? categoryId, [Bind("Id,Name,DisplayOrder")] Category category)
+        {
+            if (categoryId != category.Id)
+            {
+                return NotFound();
+            }
+
+            if (category.Name == category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("CustomError", "Value for Name and Display can't be same.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _dbContext.Categories.Update(category);
+                _dbContext.SaveChanges();
+                TempData["success"] = "Category updated successfully.";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                TempData["error"] = "Error occured, try again.";
+                return View(category);
+            }
+
+        }
+
+        // Create Category : GET
+        public IActionResult DeleteCategory(int? categoryId)
+        {
+            if (categoryId == null || categoryId == 0)
+            {
+                return NotFound();
+            }
+
+            var categoryList = _dbContext.Categories.FirstOrDefault(x => x.Id == categoryId);
+
+            if (categoryList == null)
+            {
+                return NotFound();
+            }
+            return View(categoryList);
+        }
+
+        // Create Category : Post
+        [HttpPost, ActionName("DeleteCategory")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteCategoryConfirm(int id)
+        {
+            var categoryList = _dbContext.Categories.Find(id);
+
+            if (categoryList == null) { return NotFound(); }
+            try
+            {
+                _dbContext.Categories.Remove(categoryList);
+                _dbContext.SaveChanges();
+                TempData["success"] = "Category deleted successfully.";
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                TempData["error"] = "Error occured, try again.";
+                return RedirectToAction("DeleteCategory", new { categoryId = id });
+            }
+
+        }
     }
 }
--- BookLibraryManager/Models/Category.cs
+++ BookLibraryManager/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BookLibraryManager.Models;
 
@@ -8,6 +9,9 @@ public class Category
     public int Id { get; set; }
     [Required]
     public string Name { get; set; }
+
+    [DisplayName("Display Order")]
+    [Range(1, 100, ErrorMessage = "Display range should be between 1 and 100")]
     public int DisplayOrder { get; set; }
     public DateTime CreatedDateTime { get; set; } = DateTime.Now;
 }
